5d88e065fc4cd49801494703b5d3d3d5dcc448eb
aryan <guptaryan73@gmail.com>
2022-02-22 17:21:21 +0530

    a

--- hangman.c
+++ hangman.c
@@ -1,36 +1,27 @@
-#include<stdio.h>    /*Header file declaration*/
-
-#include<string.h>   /*<string.h> for strcmp();,strlen(); functions use*/
-
-#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
 
 void showHangman(int);
 
 int main(void)
 {
-    char hangmanWord[100], tempWord[100];       /**hangmanWord[] array for the original word and tempWord[] array to get the alphabet from user and compare it with original word**/
-    char hangmanOutput[100];                    /**This array will show the remaining blanks and correct inputs**/
-    int wrongTry = 6 , matchFound = 0;          /**player will get 5 chance, so we use wrongTry as chance counter**/
-                                                /**matchFound to search the alphabet, if the alphabet from user does not exist
-                                                in the original word it will remain 0, upon finding the word, matchFound will
-                                                be set as 1**/
-    int counter = 0 , position = 0, winner, length , i;
-    char alphabetFromUser;
+    char tempWord[100], hangmanOutput[100], alphabetFromUser, hangmanWord[100]="hello";    
+      
+    /* hangmanWord[] array is used for the original word and tempWord[] array is used to get the alphabet from user and compare it with original word */
 
-    system("cls");                              /**for clearing the screen**/
-    printf("\n\n Enter any word in small case and hit >>ENTER<<");
-    printf("\n\n\t Enter HERE ==>  ");
-    scanf("%s",hangmanWord);                    /**get the string from opponent**/
-    printf("\n\n Now give the COMPUTER to your friend and see if he/she can CRACK it!!!");
+    /* alphabetFromUser array is used to show the remaining blanks and correct inputs */
+    int wrongTry = 6 , matchFound = 0, counter = 0 , position = 0, winner, length , i;          
+    /* Player will get 5 chance, so we use int wrongTry as chance counter updater */
+
+    /** int matchFound is used to search the alphabet and if the alphabet from user does not exist in the original word, it will remain 0 and upon finding the word, matchFound will be set as 1 **/
+
     printf("\n\n\tHIT >>ENTER<<");
-    getchar();                                  /**hold the computer screen**/
-    length = strlen(hangmanWord);               /**get the length of the word**/
+    getchar();                                  /*hold the computer screen*/
+    length = strlen(hangmanWord);               /*get the length of the word*/
 
-    system("cls");
 
-    printf("\n\n !!!!!!!!!!!!!!!!!!!Welcome to the HANGMAN GAME!!!!!!!!!!!!!!!!!\n\n\n");   /**Brief description of the game**/
+    printf("\n\n !!!!!!!!!!!!!!!!!!!Welcome to the HANGMAN GAME!!!!!!!!!!!!!!!!!\n\n\n");
     printf("\n\n You will get 5 chances to guess the right word");
-    printf("\n\n So help the Man and get...set...GO..!!");
 
     getchar();
 
@@ -38,16 +29,15 @@ int main(void)
 
     getchar();
 
-    system("cls");
 
-        printf("\n\t||===== ");                 /**show the HANGMAN**/
+        printf("\n\t||===== ");                 /**to show the HANGMAN**/
 	printf("\n\t||    | ");
         printf("\n\t||      ");
         printf("\n\t||      ");
         printf("\n\t||      ");
         printf("\n\t||      ");
 
-    printf("\n\n     The word has %d alphabets \n\n",length);  /**tell the user how many alphabets the word has**/
+    printf("\n\n     The word has %d alphabets \n\n",length);  /** to tell the user how many alphabets the word has**/
     for( i = 0; i < length ; i++)
     {
         hangmanOutput[i] = '_';
@@ -57,176 +47,159 @@ int main(void)
     for(i = 0 ; i < length ; i++)
     {
         printf(" ");
-        printf("%c",hangmanOutput[i]);        /**Show the Word With n(length of the original word) number of underscores (_)**/
+        printf("%c",hangmanOutput[i]);        /** to show the word With n(length of the original word) number of underscores (_)**/
 
     }
-    while(wrongTry != 0)                        /**while loop for exiting the program when no try left**/
+    while(wrongTry != 0)                        /**while loop for exiting the program when no tries are left**/
     {
         matchFound = 0;
         printf("\n\n   enter any alphabet from a to z and please use small case!!");
         printf("\n\n\t Enter HERE ==> ");
 
-	    fflush(stdin);
-
-	    scanf("%c",&alphabetFromUser);            /**get alphabet from user**/
-    if(alphabetFromUser < 'a' || alphabetFromUser > 'z') /**In case player gives input other than 'a' to 'z' the console will ask again**/
+	    scanf("%c",&alphabetFromUser);            /* to get alphabet from user */
+    if(alphabetFromUser < 'a' || alphabetFromUser > 'z') /** In case player gives input other than 'a' to 'z' the console will ask again**/
     {
-        system("cls");
-        printf("\n\n\t Wrong input TRY AGAIN ");
+        printf("\n\n\t invalid input, please enter an aplhabet ");
         matchFound = 2;
     }
-    fflush(stdin);
     if (matchFound != 2)
     {
-        for(counter=0;counter<length;counter++)    /**for loop to check whether player input alphabet exists or not in the word**/
+        for(counter=0;counter<length;counter++)    /*for loop to check whether player input alphabet exists or not in the word**/
 	    {
 		    if(alphabetFromUser==hangmanWord[counter])
 		     {
 		       matchFound = 1;
-                     }//end of if()
-            }//end of for()
+                     }
+            }
 
-	   if(matchFound == 0)                      /**in case of wrong guess**/
+	   if(matchFound == 0)                      /**in case of wrong guess from the user **/
 	    {
      	      printf("\n\t :( You have %d tries left ",--wrongTry);
 	          getchar();
               showHangman(wrongTry);
               getchar();
-	    }//end of if()
+	    }
 
 	   else
 	   {
-	     for(counter = 0; counter < length; counter++)
+	        for(counter = 0; counter < length; counter++)
              {
-     	         matchFound = 0;
-                 if(alphabetFromUser == hangmanWord[counter])
-	          {
-     		     position = counter ;
-     		     matchFound = 1;
-	          }//end of if
+     	        matchFound = 0;
+                if(alphabetFromUser == hangmanWord[counter])
+	            {
+     		        position = counter ;
+     		        matchFound = 1;
+	            }
     	      if(matchFound == 1)
 	          {
-                 for(i = 0 ; i < length ; i++)
-                 {
+                    for(i = 0 ; i < length ; i++)
+                    {
                       if( i == position)
                   	  {
-                          hangmanOutput[i] = alphabetFromUser; /**Put the alphabet at right position**/
+                            hangmanOutput[i] = alphabetFromUser; /**Put the alphabet at right position**/
                       }
-                      else if( hangmanOutput[i] >= 'a' && hangmanOutput[i] <= 'z' ) /** If the position already occupied
-                                                                                  by same alphabet then no need to
-                                                                                  fill again EASY!! and continue */
+                      else if( hangmanOutput[i] >= 'a' && hangmanOutput[i] <= 'z' ) /** If the position already occupied by same alphabet then no need to fill it again*/
                       {
-                          continue;
+                            continue;
                   	  }
 
                       else
                       {
-                          hangmanOutput[i] = '_';            /** Put a blank at not guessed alphabet position **/
+                            hangmanOutput[i] = '_';       /** Put a blank at not guessed alphabet position **/
                       }
-                }
-                tempWord[position] = alphabetFromUser;      /**put the alphabet in another char array to check with the original word**/
-                tempWord[length] = '\0';                    /**put the NULL character at the end of the temp string**/
-                winner = strcmp(tempWord,hangmanWord);      /**upon True comparison it will return 0**/
+                    }
+                tempWord[position] = alphabetFromUser;/**put the alphabet in another char array to check with the original word**/
+                tempWord[length] = '\0'; /**put the NULL character at the end of the temp string**/
+                winner = strcmp(tempWord,hangmanWord);  /** upon a match, it will return 0 **/
 
-                if(winner == 0)                             /**if the player guessed the whole word right then he/she is the WINNER**/
+                if(winner == 0)      /**if the player guessed the whole word right then he/she is the WINNER**/
                 {
                     printf("\n\n\t \t YAHOO!!!!! You are the WINNER !!!!!");
                     printf("\n\n\t The Word was %s ",hangmanWord);
-                    printf("\n\n\n\n\t\tEASY HUH???\n\n");
                     getchar();
                     return 0;
-                }//end of inner if
-	       }//end of outer if
-	    }//end of for loop
-      }//end of else
-     }// end of if(matchFound != 2) condition
+                }
+	       }
+	    }
+      }
+     }
 
     printf("\n\n\t");
     for(i = 0 ; i < length ; i++)
-      {
-          printf(" ");
-          printf("%c",hangmanOutput[i]);                /**Show the original Word With blanks and right Input alphabet**/
-      }
+    {
+        printf(" ");
+        printf("%c",hangmanOutput[i]);   /** to Show the original Word With blanks and right Input alphabet**/
+    }
 
     getchar();
-    }//end of while loop
+    }
 
-      if(wrongTry <= 0)                                 /**if the player can not guess the whole word in 5 chaces**/
+      if(wrongTry <= 0)       /**if the player can not guess the whole word in 5 chances**/
       {
           printf("\n\n\t The Word was %s ",hangmanWord);
-          printf("\n\n\t The man is dead you IDIOT!!!!!");
 	      printf("\n\n\t Better luck next!!!");
 
       }
 getchar();
 return 0;
-}//end of main();
+}
 
 
 
-void showHangman(int choice)                            /**This function show the hangman after each wrong try**/
+void showHangman(int choice)           /**This function is to show the hangman figure after each wrong try**/
  {
 
-     switch(choice)
-     {
+    switch(choice)
+    {
 
      case 0:
-         system("cls");
-	printf("\n\t||===== ");
-	printf("\n\t||    | ");
-	printf("\n\t||   %cO/",'\\');
-	printf("\n\t||    | ");
-	printf("\n\t||   / %c",'\\');
-	printf("\n\t||      ");
-	break;
+	    printf("\n\t||===== ");
+	    printf("\n\t||    | ");
+	    printf("\n\t||   %cO/",'\\');
+	    printf("\n\t||    | ");
+	    printf("\n\t||   / %c",'\\');
+	    printf("\n\t||      ");
+	    break;
      case 1:
-         system("cls");
-	printf("\n\t||===== ");
-	printf("\n\t||    | ");
-	printf("\n\t||   %cO/",'\\');
-	printf("\n\t||    | ");
-	printf("\n\t||     %c",'\\');
-	printf("\n\t||      ");
-	break;
+	    printf("\n\t||===== ");
+	    printf("\n\t||    | ");
+	    printf("\n\t||   %cO/",'\\');
+	    printf("\n\t||    | ");
+	    printf("\n\t||     %c",'\\');
+	    printf("\n\t||      ");
+	    break;
      case 2:
-         system("cls");
-	printf("\n\t||===== ");
-	printf("\n\t||    | ");
-	printf("\n\t||   %cO/",'\\');
-	printf("\n\t||    | ");
-	printf("\n\t||      ");
-	printf("\n\t||      ");
-	break;
+	    printf("\n\t||===== ");
+	    printf("\n\t||    | ");
+	    printf("\n\t||   %cO/",'\\');
+	    printf("\n\t||    | ");
+	    printf("\n\t||      ");
+	    printf("\n\t||      ");
+	    break;
      case 3:
-         system("cls");
-	printf("\n\t||===== ");
-	printf("\n\t||    | ");
-	printf("\n\t||   %cO/",'\\');
-	printf("\n\t||      ");
-	printf("\n\t||      ");
-	printf("\n\t||      ");
-	break;
+	    printf("\n\t||===== ");
+	    printf("\n\t||    | ");
+	    printf("\n\t||   %cO/",'\\');
+	    printf("\n\t||      ");
+	    printf("\n\t||      ");
+	    printf("\n\t||      ");
+	    break;
      case 4:
-         system("cls");
-	printf("\n\t||===== ");
-	printf("\n\t||    | ");
-	printf("\n\t||   %cO ",'\\');
-	printf("\n\t||      ");
-	printf("\n\t||      ");
-	printf("\n\t||      ");
-	break;
+	    printf("\n\t||===== ");
+	    printf("\n\t||    | ");
+	    printf("\n\t||   %cO ",'\\');
+	    printf("\n\t||      ");
+	    printf("\n\t||      ");
+	    printf("\n\t||      ");
+	    break;
      case 5:
-         system("cls");
-	printf("\n\t||===== ");
-	printf("\n\t||    | ");
-	printf("\n\t||    O ");
-	printf("\n\t||      ");
-	printf("\n\t||      ");
-	printf("\n\t||      ");
-	break;
-      }//end of switch-case
+	    printf("\n\t||===== ");
+	    printf("\n\t||    | ");
+	    printf("\n\t||    O ");
+	    printf("\n\t||      ");
+	    printf("\n\t||      ");
+	    printf("\n\t||      ");
+	    break;
+      }
       return;
- }
-
-
-
+}
